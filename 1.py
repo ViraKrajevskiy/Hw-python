e149c61fa5c17da43d7a94bb58b23ad1e03d472e
vira <156062320+ViraKrajevskiy@users.noreply.github.com>
2024-12-25 18:29:57 +0500

    home work

---
+++ 1.py
@@ -0,0 +1,18 @@
+class FileManager:
+    def __init__(self, filename, mode):
+        self.filename = filename
+        self.mode = mode
+        self.file = None
+
+    def __enter__(self):
+        self.file = open(self.filename, self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.file:
+            self.file.close()
+
+with FileManager("example.txt", "w") as file:
+    file.write("Привет, мир!")
+
+print("Файл успешно записан!")
